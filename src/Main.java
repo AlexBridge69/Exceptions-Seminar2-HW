828901c7678bf4718f78fc73278bc955d8497a3e
Alexander Chubanov <chubanov.alexander@yandex.ru>
2022-11-09 00:25:15 +0300

    Каким-то образом я смог заставить игнорировать при сложении ячейки со String, который не содержит чисел.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String[][] array2d = {{"1", "2", "3", "4", "5"}, {"6", "7", "8", "9"}};
+        String[][] array2d = {{"1", "2", "3", "4", "5"}, {"6", "7", "8", "9", "a"}};
         int sum = sum2d(array2d);
         System.out.println("The sum of the elements of a 2D array = " + sum + ".");
     }
@@ -10,6 +10,7 @@ public class Main {
         for (int i = 0; i < a.length - 1; i++) {
             IsRowsEqual(a, i);
         }
+
         int s = 0;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < 5; j++) {
@@ -23,9 +24,6 @@ public class Main {
         return s;
     }
 
-    /*
-        Вывел определение корректности типа данных в ячейке в отдельную функцию.
-    */
     public static boolean isIntegerString(String number) {
         try {
             Integer.parseInt(number);
@@ -34,6 +32,7 @@ public class Main {
             return false;
         }
     }
+
     public static void IsRowsEqual(String[][] array, int rowIndex) {
         if (array[0].length != array[rowIndex + 1].length) {
             throw new RuntimeException("Rows has different lengths.");
